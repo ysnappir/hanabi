e7efb6c99183a51cc87811ab48616219e2cee5fc
ysnappir <ysnappir@trigovision.com>
2020-03-23 07:58:34 +0200

    replaced numpy randperm by native random.shuffle

--- hanabi_game/hanabi_deck.py
+++ hanabi_game/hanabi_deck.py
@@ -1,12 +1,13 @@
+import random
 from typing import List
-import numpy as np
 from hanabi_game.hanabi_game_api import IHanabiDeck, IHanabiCard
 from hanabi_game.utils import get_all_cards_list
 
 
 def _create_random_card_deck() -> List[IHanabiCard]:
     all_cards_list = get_all_cards_list()
-    return [all_cards_list[i] for i in np.random.permutation(len(all_cards_list))]
+    random.shuffle(all_cards_list)
+    return all_cards_list
 
 
 class HanabiDeck(IHanabiDeck):
