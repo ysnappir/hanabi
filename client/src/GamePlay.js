3bcb4a3153091a9b410b1073a851d27fdb19422b
EthanShiloh <et.shiloh@gmail.com>
2020-03-25 22:03:34 +0200

    Added card display in the Player class

--- client/src/GamePlay.js
+++ client/src/GamePlay.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import FullTokenPile from './Tokens.js'
+import Player, {TEST_CARDS} from './Player.js'
 
+const TEST_JSON = '{"deck_size": 42, "blue_tokens": 5, "red_tokens": 2, "table": {"rainbow": 3, "blue": null, "red": 5}, "hands": [{"id": "p_0", "display_name": "Snap", "cards": [{"number": 3, "color": "red"}, {"number": 3, "color": "red"}]}, {"id": "p_1", "display_name": "Ethan", "cards": [{"number": 5, "color": "blue"}, {"number": 3, "color": "red"}]}, {"id": "p_2", "display_name": "Amos", "cards": [{"number": 2, "color": "yellow"}, {"number": 1, "color": "white"}]}], "burnt_pile": [{"number": 2, "color": "yellow"}, {"number": 2, "color": "yellow"}, {"number": 2, "color": "yellow"}]}';
 
 class GamePlay extends Component {
     constructor (props) {
         super(props)
+        this.state = {
+            num_of_players: props.num_of_players
+        }
+        this.player = React.createRef();
+      }
+
+      componentDidMount() {
+        this.interval = setInterval(() => this.update_game(TEST_JSON), 1000);
+      }
+      componentWillUnmount() {
+        clearInterval(this.interval);
+      }
+      
+      update_game(game_json) {
+
+        this.player.current.update_cards(TEST_CARDS);
       }
 
     render () {
@@ -14,6 +32,8 @@ class GamePlay extends Component {
             Full game play <br/> <br/>
             Tokens Status: <br/>
             <FullTokenPile/>
+            <br/><br/>
+            <Player display_name='asdf' ref={this.player}/>
         </div>
         )
     }
