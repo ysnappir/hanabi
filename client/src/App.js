fbc05fbcd7d398a1c5050a60b7b6744b4bc1119b
EthanShiloh <et.shiloh@gmail.com>
2020-04-01 14:30:22 +0300

    Put Error component in different file

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,7 @@ import axios from 'axios';
 import Options from './Options';
 import {UserIdContext} from './Contex.js';
 import GamePlay from './GamePlay';
-import PropTypes from 'prop-types';
-
-
-const Error = ({ touched, message }) => {
-  if (!touched) {
-    return <div className="form-message invalid">&nbsp;</div>;
-  }
-  if (message) {
-    return <div className="form-message invalid">{message}</div>;
-  }
-  return <div className="form-message valid">all good</div>;
-};
-
-Error.propTypes = {
-  touched: PropTypes.bool.isRequired,
-  message: PropTypes.string.isRequired,
-};
+import Error from './Error';
 
 
 const ValidationSchema = Yup.object().shape({
