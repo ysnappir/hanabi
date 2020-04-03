c15d6b0a67a119dba88960a875885da6e95bb3eb
EthanShiloh <et.shiloh@gmail.com>
2020-04-03 21:20:05 +0300

    Fixed location issues in remaining deck

--- client/src/CardPiles.js
+++ client/src/CardPiles.js
@@ -5,10 +5,10 @@ function RemainingDeck(props) {
   const {remainingCards} = props;
 
   return (
-    <div style={{width: '81px', height:'125px', }}>
+    <div style={{width: '81px'}}>
       Remaining Deck: <br/>
       <img src={require ('./img/BackRect125.png')} style={{width: '100%'}}/>
-      <div style={{position: 'relative', top: '-100%', textAlign: 'center', fontFamily: 'Frijole', fontSize: '40px', 
+      <div style={{position: 'relative', top: '-30px', marginTop: '-60px', textAlign: 'center', fontFamily: 'Frijole', fontSize: '40px', 
         color: 'white', textShadow:'-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black'}}>{remainingCards}</div>
     </div>
   );
