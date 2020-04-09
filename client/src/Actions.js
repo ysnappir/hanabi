9cf73786d01f99f7aad912bdcf8101bccbbb261c
ysnappir <ysnappir@trigovision.com>
2020-04-09 13:44:20 +0300

    fixed popup and refactored actions file

--- client/src/Actions.js
+++ client/src/Actions.js
@@ -1,73 +1,32 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Popup from 'reactjs-popup';
-import axios from 'axios';
-import {UserIdContext} from './Contex.js';
+function InformPlayerOptions(props) {
+  const {onClose, showPopup, reportSelection, playerDisplayName, highlightArray} = props;
 
-function ActionsPopup(props) {
-  const {showPopup, onCloseFunc, cardIndex, playerId} = props;
+  let informationOptions = [1, 2, 3, 4, 5, 'red', 'green', 'blue', 'yellow', 'white', 'rainbow'];
 
-  let headerMsg = '';
-  
-  headerMsg = 'Pressed Player: ' + playerId;
-  
-  return (
-    <Popup trigger={<div></div>} onClose={onCloseFunc} open={showPopup}>
-      <h1> {headerMsg} </h1>
-      <DifferentPlayerActions playerId={playerId} onClose={onCloseFunc}/>
-      <button onClick={onCloseFunc}>Do nothing!</button>
-    </Popup>
-  );
-}
-
-ActionsPopup.propTypes = {
-  onCloseFunc: PropTypes.func.isRequired,
-  showPopup: PropTypes.bool.isRequired,
-  cardIndex: PropTypes.number.isRequired,
-  playerId: PropTypes.string.isRequired,
-};
-
-function DifferentPlayerActions(props) {
-  const userId = useContext(UserIdContext);
-  const {playerId, onClose} = props;
-
-  let informationOptions = [1, 2, 3, 4, 5, 'red', 'green', 'blue', 'yellow', 'white'];
-
-  const sendInform = (value) => {
-    console.log('Informing ' + playerId + ' about ' + value + '!');
-    // console.log(typeof playerId);
-    axios.post( `/make_turn/inform/${userId}`, {'informed_player_id': playerId, 'information': value});
-    onClose();
+  const renderPopUp = () => {
+    return(
+      <Popup trigger={<div></div>} onClose={onClose} open={showPopup}>
+        <h1> Inform {playerDisplayName} about: </h1>
+        {informationOptions.map((value) => <button key={value} onClick={() => reportSelection(value)}
+          style={{background: highlightArray.includes(value) ? 'yellow' : 'white'}}>{value}</button>)}
+        <br/><br/>
+        <button onClick={onClose}>Do nothing!</button><button onClick={onClose}>Think!</button>
+      </Popup>
+    );
   };
 
-  return (
-    <div>
-      {informationOptions.map((value) => <button key={value} onClick={() => sendInform(value)}>{value}</button>)}
-    </div>
-  );
+  return renderPopUp();
 }
 
-DifferentPlayerActions.propTypes = {
-  playerId: PropTypes.string.isRequired,
+InformPlayerOptions.propTypes = {
   onClose: PropTypes.func.isRequired,
+  showPopup: PropTypes.bool.isRequired,
+  reportSelection: PropTypes.func.isRequired,
+  playerDisplayName: PropTypes.string.isRequired,
+  highlightArray: PropTypes.array,
 };
 
-function Action(props) {
-  const {buttonDisplay, actionFunc, funcData, userId} = props;
-
-  return (
-    <div>
-      <button onClick={() => actionFunc(userId, funcData)}>{buttonDisplay}</button>
-    </div>
-  );
-}
-
-Action.propTypes = {
-  buttonDisplay: PropTypes.string.isRequired,
-  actionFunc: PropTypes.func.isRequired,
-  funcData: PropTypes.any.isRequired,
-  userId: PropTypes.number.isRequired,
-};
-  
-
-export default ActionsPopup;
+export default InformPlayerOptions;
