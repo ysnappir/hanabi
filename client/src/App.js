3d6c1aae893498902c16fb40762700169660f673
EthanShiloh <et.shiloh@gmail.com>
2020-04-12 23:04:59 +0300

    Starting to use material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
 import axios from 'axios';
 import Options from './Options';
 import {UserIdContext} from './Contex.js';
 import GamePlay from './GamePlay';
 
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { makeStyles } from '@material-ui/core/styles';
+
+
 const BAD_INPUT_MSG = 'Empty Display Name or Color Num not a number';
 const WAIT_STR = 'Please Wait...';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const [startLogin, setStartLogin] = useState(false);
   const [loginSuccess, setLoginSuccess] = useState(false);
   const [userMsg, setUserMsg] = useState('');
   const [userId, setUserId] = useState('');
@@ -16,6 +35,8 @@ function App() {
 
   const displayNameTextbox = React.createRef();
   const colorNumTextbox = React.createRef();
+
+  const classes = useStyles();
 
   const validateInput = () => {
     const dispNameVal = displayNameTextbox.current.value;
@@ -66,13 +87,90 @@ function App() {
   }
 
   return (
-    <div className='main__container'>
+    <Fragment>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title} align='center'>
+              Hanabi Online!
+            </Typography>
+            <IconButton 
+              color="inherit"
+              onClick={() => setStartLogin(true)}>
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </div>
+      {startLogin ? 
+        <div className='main__container'>
         Display Name: <input type="text" ref={displayNameTextbox} /><br/>
         How many colors are you wearing: <input type="text" ref={colorNumTextbox} /><br/><br/>
-      <button className='button' onClick={handleLoginClick}>Lets Start!</button>
-      <p>{userMsg}</p>
+          <button className='button' onClick={handleLoginClick}>Lets Start!</button>
+          <p>{userMsg}</p>
+        </div>
+        :
+        <div/>
+      }
+    </Fragment>
+  );
+}
+/*
+{auth && (
+  <div>
+    <IconButton
+      aria-label="account of current user"
+      aria-controls="menu-appbar"
+      aria-haspopup="true"
+      onClick={handleMenu}
+      color="inherit"
+    >
+      <AccountCircle />
+    </IconButton>
+    <Menu
+      id="menu-appbar"
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={open}
+      onClose={handleClose}
+    >
+      <MenuItem onClick={handleClose}>Profile</MenuItem>
+      <MenuItem onClick={handleClose}>My account</MenuItem>
+    </Menu>
+  </div>
+)}
+*/
+
+
+
+/*
+export default function ButtonAppBar() {
+  
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
+*/
 
 export default App;
