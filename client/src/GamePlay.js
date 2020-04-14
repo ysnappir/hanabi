ba77ebeabfaaa933cfa4df53953139d740280bf5
EthanShiloh <et.shiloh@gmail.com>
2020-04-14 16:26:54 +0300

    deleted to automatic start game hack

--- client/src/GamePlay.js
+++ client/src/GamePlay.js
@@ -361,8 +361,8 @@ function GamePlay(props) {
 
   const updateGameState = async () => {
     try {
-      //const response = await axios.get('/game_state/' + userId + '/' + gameId, {});
-      const response = '';
+      const response = await axios.get('/game_state/' + userId + '/' + gameId, {});
+      //const response = '';
       handleGetGameStateResponse(response);
     } catch (error) {
       handleGetGameStateError(error);
@@ -375,8 +375,8 @@ function GamePlay(props) {
   );
 
   const handleGetGameStateResponse = (response) => {
-    //let myJson = response.data;
-    let myJson = TEST_JSON;
+    let myJson = response.data;
+    //let myJson = TEST_JSON;
     console.log(myJson);
     setAvailableClueTokens(myJson['blue_tokens']);
     setAvailableMissTokens(myJson['red_tokens']);
